34e45f301706c8384a396d605ae872f6b5773d83
Andy Tran <rooneytai5236@gmail.com>
2024-07-05 02:45:37 +1000

    update program

--- CurrentTimeService/Program.cs
+++ CurrentTimeService/Program.cs
@@ -1,26 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.MapGet("time/utc", () => Results.Ok(DateTime.UtcNow));
 
-// map controllers
-app.MapControllers();
+await app.RunAsync();
 
-app.Run();
